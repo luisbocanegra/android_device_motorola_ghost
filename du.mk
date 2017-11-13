fc78e55efe670cde2a6c0649d843d99f4d94f670
LuisB <luisbocanegra17b@gmail.com>
2017-11-12 22:06:06 -0600

    DU-ify
    Change-Id: I510f34bf724f95593d2a94dddddfe487257964de

---
+++ du.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+PRODUCT_RELEASE_NAME := MOTO X
+PRODUCT_NAME := du_ghost
